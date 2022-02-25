f4077cb2375e04739ece8d9794d426aa709e3e87
qkrwjdgp <qkrwjdgp0213@gamil.com>
2022-02-25 14:10:58 +0900

    front

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="header">
+    <div class="header_u_af">
+      <ul class="header_u">
+        <li>로그인</li>
+        <li>회원가입</li>
+      </ul>
+    </div>
+    <h1>
+      <a href="#">
+        <img src="../../assets/img/Free_Sample_By_Wix (5).png" />
+      </a>
+    </h1>
+    <nav class="header_nev">
+      <ul class="header_nev_ul">
+        <li>커뮤니티</li>
+        <li>대외활동</li>
+        <li>공모전</li>
+        <li>스터디</li>
+        <li>게시요청</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style src="../../sass/main.css"></style>
